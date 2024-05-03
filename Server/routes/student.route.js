60a07f84bfd0c37ebbcd0c05d017af991bf028a5
Dascălu Rareș <dascaluraresm@gmail.com>
2024-05-03 18:46:12 +0300

    save

--- Server/routes/student.route.js
+++ Server/routes/student.route.js
@@ -2,35 +2,36 @@
 let mongoose = require("mongoose"),
   express = require("express"),
   router = express.Router();
+bcrypt = require("bcryptjs");
 
 // Student Model
 let studentSchema = require("../models/Student");
 
 // CREATE Student
-router.post("/create-student", (req, res, next) => {
-  // Check if a user with the same email already exists
-  studentSchema
-    .findOne({ email: req.body.email })
-    .then((user) => {
-      if (user) {
-        // If a user with the same email already exists, send an error message
-        res.status(400).send("A user with this email already exists");
-      } else {
-        // If no user with the same email exists, create a new user
-        studentSchema
-          .create(req.body)
-          .then((data) => {
-            console.log(data);
-            res.json(data);
-          })
-          .catch((error) => {
-            return next(error);
-          });
-      }
-    })
-    .catch((err) => {
-      return next(err);
+router.post("/create-student", async (req, res, next) => {
+  try {
+    // Hash and salt the password before saving to the database
+    const hashedPassword = await bcrypt.hash(req.body.password, 10); // Hash the password with salt rounds = 10
+
+    // Check if a user with the same email already exists
+    const existingUser = await studentSchema.findOne({ email: req.body.email });
+    if (existingUser) {
+      // If a user with the same email already exists, send an error message
+      return res.status(400).send("A user with this email already exists");
+    }
+
+    // If no user with the same email exists, create a new user
+    const newUser = await studentSchema.create({
+      email: req.body.email,
+      password: hashedPassword, // Save the hashed password to the database
+      // Include other fields as needed
     });
+
+    console.log("User created successfully !");
+    res.json(newUser);
+  } catch (error) {
+    return next(error);
+  }
 });
 
 // READ Students
